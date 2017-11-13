7a2cf82ba4789a51efbc9b76bf27f460949fb110
Alaa Awad <alaa.awad.mail@gmail.com>
2017-11-12 22:37:11 -0500

    new models added. old models removed

--- app/model/mnist.py
+++ app/model/mnist.py
@@ -1,42 +1,50 @@
 import tensorflow as tf
-# The MNIST data is split into three parts:
-#   55,000 data points of training data (mnist.train),
-#   10,000 points of test data (mnist.test),
-#   and 5,000 points of validation data (mnist.validation). 
-from tensorflow.examples.tutorials.mnist import input_data
-mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
 
-# Both the training set and test set contain images and their corresponding labels;
-# for example the training images are mnist.train.images and 
-# the training labels are mnist.train.labels
-# 
-# Each image is 28 pixels by 28 pixels. We can interpret this as a big array of numbers:
-x = tf.placeholder(tf.float32, [None, 784],name="x")
-W = tf.Variable(tf.zeros([784, 10]),name='W')
-b = tf.Variable(tf.zeros([10]),name='b')
+# Softmax Regression Model
+def regression(x):
+    W = tf.Variable(tf.zeros([784, 10]), name="W")
+    b = tf.Variable(tf.zeros([10]), name="b")
+    y = tf.nn.softmax(tf.matmul(x, W) + b)
+    return y, [W, b]
 
-y = tf.nn.softmax(tf.matmul(x,W) + b, name='y')
-y_ = tf.placeholder(tf.float32, [None, 10])
 
-cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
+# Multilayer Convolutional Network
+def convolutional(x, keep_prob):
+    def conv2d(x, W):
+        return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
 
-train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+    def max_pool_2x2(x):
+        return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
 
-sess = tf.InteractiveSession()
-saver = tf.train.Saver()
-tf.global_variables_initializer().run()
+    def weight_variable(shape):
+        initial = tf.truncated_normal(shape, stddev=0.1)
+        return tf.Variable(initial)
 
-for _ in range(100):
-    batch_xs, batch_ys = mnist.train.next_batch(100)
-    sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+    def bias_variable(shape):
+        initial = tf.constant(0.1, shape=shape)
+        return tf.Variable(initial)
 
-correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-
-print('model accuracy:',sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
-
-save_path = saver.save(sess, "./v1.00/model.ckpt")
-print("Model saved in file: %s" % save_path)
-
-sess.close()
+    # First Convolutional Layer
+    x_image = tf.reshape(x, [-1, 28, 28, 1])
+    W_conv1 = weight_variable([5, 5, 1, 32])
+    b_conv1 = bias_variable([32])
+    h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
+    h_pool1 = max_pool_2x2(h_conv1)
+    # Second Convolutional Layer
+    W_conv2 = weight_variable([5, 5, 32, 64])
+    b_conv2 = bias_variable([64])
+    h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
+    h_pool2 = max_pool_2x2(h_conv2)
+    # Densely Connected Layer
+    W_fc1 = weight_variable([7 * 7 * 64, 1024])
+    b_fc1 = bias_variable([1024])
+    h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
+    h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+    # Dropout
+    h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+    # Readout Layer
+    W_fc2 = weight_variable([1024, 10])
+    b_fc2 = bias_variable([10])
+    y = tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
+    return y, [W_conv1, b_conv1, W_conv2, b_conv2, W_fc1, b_fc1, W_fc2, b_fc2]
